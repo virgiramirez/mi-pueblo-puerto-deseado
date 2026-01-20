a204d243170fa40220292bbe7214428144304c47
Virginia <virginiasr19@gmail.com>
2026-01-20 18:50:03 -0300

    Agrega navegación por páginas y formulario de reclamos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("Mi Pueblo - Puerto Deseado cargado correctamente");
+const form = document.getElementById("reclamoForm");
+
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const nombre = document.getElementById("nombre").value;
+    const descripcion = document.getElementById("descripcion").value;
+
+    document.getElementById("mensaje").textContent =
+      `Gracias ${nombre}, tu reclamo fue registrado.`;
+
+    form.reset();
+  });
+}
